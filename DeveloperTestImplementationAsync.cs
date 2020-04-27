7e421afc6b0d7035950867a37d3ca4e4267e77ed
Lyubomir Velchev <lyubomir.velchev@gmail.com>
2020-04-27 08:06:39 +0100

    solution for the 2 questions; added comment in ReadMe.txt

--- DeveloperTestImplementationAsync.cs
+++ DeveloperTestImplementationAsync.cs
@@ -5,23 +5,119 @@
 // without the express permission of a director of Exclaimer Ltd
 // ---------------------------------------------------------------
 #endregion
-using System;
-using System.Threading.Tasks;
 
-using DeveloperTestInterfaces;
 
 namespace DeveloperTest
 {
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System;
+    using System.Threading.Tasks;
+    using DeveloperTestInterfaces;
+
     public sealed class DeveloperTestImplementationAsync : IDeveloperTestAsync
     {
-        public Task RunQuestionOne(ICharacterReader reader, IOutputResult output)
+        private readonly WordsDictionary _dictionary = new WordsDictionary();
+        private readonly object _locker = new object();
+        private bool _finished;
+        public async Task RunQuestionOne(ICharacterReader reader, IOutputResult output)
         {
-            throw new NotImplementedException();
+            // need fresh dictionary
+            _dictionary.Clean();
+            await ProcessReaderAsync(reader);
+            await Print(output);
         }
 
-        public Task RunQuestionTwo(ICharacterReader[] readers, IOutputResult output)
+        public async Task RunQuestionTwo(ICharacterReader[] readers, IOutputResult output)
         {
-            throw new NotImplementedException();
+            // need fresh dictionary
+            _dictionary.Clean();
+
+            var tasks = readers.Select(ProcessReaderAsync).ToList();
+            var printCounterTask = new Task(() => DelayedPrint(output));
+            printCounterTask.Start();
+
+            Task.WhenAll(tasks).ContinueWith(x => Print(output).ContinueWith(_ =>
+            {
+                lock (_locker)
+                {
+                    //mark flag to true to signal that the delayed printing need to stop
+                    _finished = true;
+                }
+            })).Wait();
+
+            printCounterTask.Wait();
+        }
+
+        private async Task ProcessReaderAsync(ICharacterReader reader)
+        {
+            var buffer = new StringBuilder();
+            try
+            {
+                do
+                {
+                    var res = reader.GetNextChar();
+                    if (res.IsLetter() || res.IsAcceptedSymbol())
+                    {
+                        if (buffer.Length > 0 &&
+                            (buffer.ToString()[buffer.ToString().Length - 1].IsAcceptedSymbol() && res.IsAcceptedSymbol())
+                        )
+                        {
+                            buffer.Remove(buffer.Length - 1, 1);
+
+                            _dictionary.Add(buffer.ToString());
+                            buffer.Clear();
+                            continue;
+                        }
+                        buffer.Append(res);
+                    }
+                    else
+                    {
+                        _dictionary.Add(buffer.ToString());
+                        buffer.Clear();
+                    }
+                } while (true);
+
+            }
+            catch (EndOfStreamException)
+            {
+                _dictionary.Add(buffer.ToString());
+            }
+        }
+
+        private Task Print(IOutputResult output)
+        {
+            foreach (var keyValuePair in _dictionary.Dictionary
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key))
+            {
+                {
+                    output.AddResult($"{keyValuePair.Key} - {keyValuePair.Value}");
+                }
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private async Task DelayedPrint(IOutputResult output)
+        {
+            while (true)
+            {
+                if (!_finished)
+                {
+                    //every 10 seconds should print. 
+                    Task.Delay(10 * 1000).Wait();
+                    await Print(output);
+                }
+                else
+                {
+#if DEBUG
+                    Console.WriteLine("Finished with printing");
+#endif
+                    break;
+                }
+            }
         }
     }
 }
---
+++ WordsDictionary.cs
@@ -0,0 +1,29 @@
+﻿namespace DeveloperTest
+{
+    using System.Collections.Generic;
+
+    public class WordsDictionary
+    {
+        public void Add(string word)
+        {
+            var key = word.ToLower().Trim();
+            if (key.Length > 0)
+            {
+                if (Dictionary.ContainsKey(key))
+                {
+                    Dictionary[key] = Dictionary[key] + 1;
+                }
+                else
+                {
+                    Dictionary.Add(key, 1);
+                }
+            }
+        }
+
+        public void Clean()
+        {
+            Dictionary = new Dictionary<string, int>();
+        }
+        public Dictionary<string, int> Dictionary { get; private set; } = new Dictionary<string, int>();
+    }
+}
